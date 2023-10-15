e74a07492270675f22472730fc26992e241d4cd2
fprebolledo <fprebolledo@uc.cl>
2023-10-14 23:22:07 -0300

    feat(routes/users): use user service in post route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,29 +1,17 @@
 import { Router } from 'express'
-import type { Request, Response } from 'express'
-import { PrismaClient } from '@prisma/client'
-import { hashPassword } from '../utils/password'
-import { type User } from '../types/user'
+import { users } from '../services/users'
+import { type UserForm } from '../types/user'
+import type { ApiRequest, Response } from '../types/request'
 
 const router = Router()
-const prisma = new PrismaClient()
 
-interface ExtendedRequest extends Request {
-  body: Omit<User, 'id'>
-}
-
-// Hacer try catch
-router.post('/', async (req: ExtendedRequest, res: Response) => {
-  // extract this to a custom model: https://www.prisma.io/docs/concepts/components/prisma-client/custom-models
-  const existingUser = await prisma.users.findFirst({
-    where: {
-      OR: [{ email: req.body.email }, { username: req.body.username }]
-    }
-  })
-  if (existingUser !== null) return res.status(401).send({ message: 'Unauthorized' })
-
-  const password = hashPassword(req.body.password)
-  const user = await prisma.users.create({ data: { ...req.body, password } })
-  return res.status(201).send({ user })
+router.post('/users', async (req: ApiRequest<UserForm>, res: Response) => {
+  try {
+    const signUpUser = await users.signUp(req.body)
+    res.status(201).json(signUpUser)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 export default router
